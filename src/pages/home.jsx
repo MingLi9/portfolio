8d4209637b910611979fee9cc1357f32b1deb504
Liroy99 <mingjanssen999@gmail.com>
2025-02-05 16:36:31 +0100

    SonalClound compliant
    Updating the code to follow SonarCloud standards

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -16,40 +16,38 @@ function Home() {
   } = UseScrollTracker();
 
   return (
-    <>
-      <div className="app">
-        <div
-          className="container"
-          ref={containerRef}
-          onScroll={handleScroll}
-        >
-          <section className="section section1">
-            <ProjectCard img={ProgrammingIMG} alt="Project preview" side="Left" title={programTitle} text={programText} cv={true} />
-          </section>
-          <section className="section section2">
-            <ProjectCard side="Center" title={loremIpsumTitle} text={loremIpsumText} />
-          </section>
-          <section className="section section3">
-            <ProjectCard img={ProgrammingIMG} alt="Project preview" side="Right" title={programTitle} text={programText} socials={true} />
-          </section>
-          <section className="section section1">
-            <h1>Section 1</h1>
-          </section>
-          <section className="section section2">
-            <h1>Section 2</h1>
-          </section>
-          <section className="section section3">
-            <h1>Section 3</h1>
-          </section>
-        </div>
-        <ScrollIndicator
-          scrollThumbHeight={scrollThumbHeight}
-          scrollThumbTop={scrollThumbTop}
-          sectionCount={sectionCount}
-          currentSection={currentSection}
-        />
+    <div className="app">
+      <div
+        className="container"
+        ref={containerRef}
+        onScroll={handleScroll}
+      >
+        <section className="section section1">
+          <ProjectCard img={ProgrammingIMG} alt="Project preview" side="Left" title={programTitle} text={programText} cv={true} />
+        </section>
+        <section className="section section2">
+          <ProjectCard side="Center" title={loremIpsumTitle} text={loremIpsumText} />
+        </section>
+        <section className="section section3">
+          <ProjectCard img={ProgrammingIMG} alt="Project preview" side="Right" title={programTitle} text={programText} socials={true} />
+        </section>
+        <section className="section section1">
+          <h1>Section 1</h1>
+        </section>
+        <section className="section section2">
+          <h1>Section 2</h1>
+        </section>
+        <section className="section section3">
+          <h1>Section 3</h1>
+        </section>
       </div>
-    </>
+      <ScrollIndicator
+        scrollThumbHeight={scrollThumbHeight}
+        scrollThumbTop={scrollThumbTop}
+        sectionCount={sectionCount}
+        currentSection={currentSection}
+      />
+    </div>
   )
 };
 
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -20,10 +20,7 @@ const Project = () => {
                 <a href="https://Strato.com" rel="noreferrer" target="_blank">Strato.com</a>
             </p>
             <p>
-                With deployment to strato.com you need to use the&nbsp;
-                <a href="https://www.strato-hosting.co.uk/faq/hosting/what-are-the-paths-on-your-system/#absoluter_pfad" rel="noreferrer" target="_blank">absolute path</a>
-                &nbsp;figuring this out is a bit tricky. You need to go "Je pakket" -&gt; "Overzicht" and scroll down untill you find a field that says "Webspace Pad".
-                This is the absolute path to your root folder.
+                With deployment to strato.com you need to use the&nbsp;<a href="https://www.strato-hosting.co.uk/faq/hosting/what-are-the-paths-on-your-system/#absoluter_pfad" rel="noreferrer" target="_blank">absolute path</a>&nbsp;figuring this out is a bit tricky. You need to go "Je pakket" -&gt; "Overzicht" and scroll down untill you find a field that says "Webspace Pad". This is the absolute path to your root folder.
             </p>
             <p>
                 <Link to="/projects/audio_controller">DIY Audio Controller</Link>
